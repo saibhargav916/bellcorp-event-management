55b2a99d894182f832e5e6bc276c1a4896c15cff
saibhargav916 <saibhargav916@gmail.com>
2026-02-16 01:12:58 +0530

    Added production API URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,17 +1,18 @@
-<<<<<<< HEAD
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://bellcorp-event-management-b8os.onrender.com/api"
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 export default API;
-=======
-import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-});
 
-export default API;
->>>>>>> 9dbec2931f522b93ea3ea727b1b5a615b8705091
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+// export default API;
+
